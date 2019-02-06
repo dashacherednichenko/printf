ec7fed540a0e9eb9a7ef6e8f0b97a70877c043dd
dasha.cherednichenko <dasha.cherednichenko@gmail.com>
2019-02-06 14:23:18 +0200

    norminatte x type, without hh flags and else

--- ft_octal.c
+++ ft_octal.c
@@ -36,46 +36,29 @@ int	ft_unsig(long long int d, t_flags *f)
 	return (i);
 }
 
-//int	ft_hex_low(long long int d, t_flags *f)
-//{
-//	char	*str;
-//	int		i;
-//
-//	ft_putstr(str = ft_strlowcase(ft_itoa_base(d, 16)));
-//	i = ft_strlen(str);
-//	f->min = 0;
-//	free(str);
-//	return (i);
-//}
-
 int	ft_hex(long long int d, t_flags *f)
 {
-	char 	*str;
-	char 	*tmp;
+	char	*s;
+	char	*tmp;
 	int		i;
-	char 	c;
 
-	str = ft_itoa_base(d, 16);
-	f->resh == 1? str = ft_strjoinfree("0X", str, 2) : 0;
-	i = ft_strlen(str);
-	if (f->min == 1)
-	{
-		f->w = f->w < 0 ? -f->w : f->w;
-		f->zr = 0;
-	}
-	c = f->zr == 1 ? '0' : ' ';
+	s = ft_itoa_base(d, 16);
+	(f->tchn_t == 1 && !f->tchn && d == 0 && f->w) ? s[0] = ' ' : 0;
+	i = ft_strlen(s);
+	(f->tchn_t == 1 || f->min == 1) ? f->zr = 0 : 0;
+	(f->tchn_t == 1 && f->tchn > i) ? tmp = ft_strtemp(f->tchn, i, '0') : 0;
+	(f->tchn_t == 1 && f->tchn > i) ? s = ft_strjoinfree(tmp, s, 3) : 0;
+	f->resh == 1 && d != 0 && !f->zr ? s = ft_strjoinfree("0X", s, 2) : 0;
+	i = ft_strlen(s);
+	(f->min == 1 && f->w < 0) ? f->w = -f->w : 0;
+	if (f->tchn_t == 1 && !f->tchn && d == 0 && !f->w)
+		return (0);
 	if (f->w > i)
 	{
-		tmp = ft_memalloc(f->w-- - i + 1);
-		while (f->w - i >=0)
-		{
-			tmp[f->w-- - i] = c;
-		}
-		str =  f->min == 1 ? ft_strjoinfree(str, tmp, 3) : ft_strjoinfree(tmp, str, 3);
+		tmp = f->zr == 1 ? ft_strtemp(f->w, i, '0') : ft_strtemp(f->w, i, ' ');
+		s = f->min == 1 ? ft_strjoinfree(s, tmp, 3) : ft_strjoinfree(tmp, s, 3);
+		f->resh == 1 && d != 0 && f->zr == 1 ? s[1] = 'X' : 0;
 	}
-	str = f->type == 'X' ? str :  ft_strlowcase(str);
-	ft_putstr(str);
-	i = ft_strlen(str);
-	free(str);
-	return (i);
+	s = f->type == 'X' ? s : ft_strlowcase(s);
+	return (ft_printnbr(s));
 }
